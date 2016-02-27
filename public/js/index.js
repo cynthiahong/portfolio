6b45553dc8712b680daa594090bd5c6b95cbce51
Cynthia Hong <cynthiahong911@gmail.com>
2016-02-27 00:32:36 -0800

    added template for home page

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,29 @@
+//navigation open
 $(function() {
   $('.nav-toggle').on('click', function() {
     $('.main-navigation').toggleClass('open')
   });
+//navigation collapse
+//$('#nav1,#nav2,#nav3').click(function() {
+//   $('.main-navigation').toggle();
+
+// });
+
 });
+
 $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
-    $('.main-navigation'.toggleClass('close'));
 	});
+  $('#nav1,#nav3').click(function(){
+		$('.main-navigation').hide();
+	});
+  $('#nav-icon1').click(function(){
+    $('.main-navigation').show();
+    $(this).toggleClass('close');
+  });
+
+  $('#btn').click(function(){
+    $('btn').delay("slow");
+  });
 });
