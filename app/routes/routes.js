44b7e5be551950527af96d95bc6dfe120c63a622
Cynthia Hong <cynthiahong911@gmail.com>
2016-02-28 01:12:35 -0800

    added first project, with some hover color

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,13 @@
-/* var data = require("../../data.json"); */
 
 module.exports = function (app) {
 	app.get('/', function(req, res){
-		res.render('index'); /*
-		res.render('index', data);*/
+		res.render('index');
 	});
 
+	app.get('/projects', function(req, res){
+		res.render('projects');
+	});
+	app.get('/pierre', function(req, res){
+		res.render('pierre');
+	});
 }
