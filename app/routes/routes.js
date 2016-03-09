e762b2834c6c73fac8f402f14e8a2ca7d1d77aa2
Cynthia Hong <cynthiahong911@gmail.com>
2016-03-09 12:51:15 -0800

    update

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,7 +7,15 @@ module.exports = function (app) {
 	app.get('/projects', function(req, res){
 		res.render('projects');
 	});
+
 	app.get('/pierre', function(req, res){
 		res.render('pierre');
+	});
+
+	app.get('/portfolio_project', function(req, res){
+		res.render('portfolio_project');
+	});
+	app.get('/mobile_playlist', function(req, res){
+		res.render('mobile_playlist');
 	});
 }
--- server.js
+++ server.js
@@ -18,23 +18,8 @@ app.set('port', process.env.PORT || 3000);
 
 // Load routes
 require('./app/routes/routes')(app);
-/*
-var ucsd = require('./app/routes/ucsd');
-app.get('./views/ucsd', ucsd.view);
-var about = require('./app/routes/about');
-app.get('./views/about', about.view);
-var faq = require('./app/routes/faq');
-app.get('./views/faq', faq.view);
-var contactus = require('./app/routes/contactus');
-app.get('.views/contactus', contactus.view);
-var stanford = require('./app/routes/stanford');
-app.get('./views/stanford', stanford.view);
-var harvard = require('./app/routes/harvard');
-app.get('./views/harvard', harvard.view);
-var appointments = require('./app/routes/appointments');
-app.get('./views/appointments', appointments.view);
 
-*/
+
 /*
 var json = require('./data.json');
 app.get('/', function(req,res){
